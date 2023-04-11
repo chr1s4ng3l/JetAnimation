080cc552b73b96353459c695f004af62f08f27b8
Consultant <christopher.tamayocazarez@gmail.com>
2023-04-11 15:58:54 -0400

    Simple Animation

--- app/src/main/java/com/tamayo/jetanimation/MainActivity.kt
+++ app/src/main/java/com/tamayo/jetanimation/MainActivity.kt
@@ -3,13 +3,23 @@ package com.tamayo.jetanimation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.tamayo.jetanimation.ui.theme.JetAnimationTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    ColorAnimationSimple()
                 }
             }
         }
@@ -30,14 +40,45 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun ColorAnimationSimple() {
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JetAnimationTheme {
-        Greeting("Android")
+    var firstColor by rememberSaveable {
+        mutableStateOf(false)
     }
+    var secondColor by rememberSaveable {
+        mutableStateOf(false)
+    }
+
+    var showBox by rememberSaveable {
+        mutableStateOf(true)
+    }
+
+    val realColor = if (firstColor) Color.Green else Color.Red
+    val realColor2 by animateColorAsState(
+        targetValue = if (secondColor) Color.Yellow else Color.Magenta,
+        animationSpec = tween(2000),
+        finishedListener = { showBox = false }
+    )
+
+
+    Column {
+        Box(modifier = Modifier
+            .size(100.dp)
+            .background(realColor)
+            .clickable { firstColor = !firstColor }
+            .clip(RoundedCornerShape(12.dp)))
+
+
+        Spacer(modifier = Modifier.size(200.dp))
+
+        if (showBox) {
+            Box(modifier = Modifier
+                .size(100.dp)
+                .background(realColor2)
+                .clickable { secondColor = !secondColor }
+                .clip(RoundedCornerShape(12.dp)))
+        }
+    }
+
+
 }
